521bf40ba03fd3e18ad86646a42085f38b0dc248
MansiDhumal25 <mansidhumal55@gmal.com>
2021-04-05 10:38:48 +0530

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/bc/c0861ab5e692001b1428b787ebf23e5b
@@ -0,0 +1,7 @@
+package practise.springboot.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String>{
+	
+}
---
+++ course-api-data/src/test/java/practise/springboot/CourseApiDataApplicationTests.java
@@ -0,0 +1,13 @@
+package practise.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
